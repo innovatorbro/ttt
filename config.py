384deff7e05b2ac05ae8c35555ea8153472ac03f
innovatorbro <108291686+innovatorbro@users.noreply.github.com>
2023-10-30 11:13:35 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+api_id = 27311412
+api_hash = 'dbd53771cb8bddd12c030f3280009c68'
+session_name = 'session'
+
+image_filename = 'time_image.jpg'
+photo_filename = '/resources/photo.png'
+image_text_color = 'white' # '#ff0800'
+image_border_color = 'black'
+image_border_size = 3
+
+profile_bio = '{}%'
